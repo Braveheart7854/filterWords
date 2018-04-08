f1e522b80f44a39f21df4f1ab54d1ed78028862c
tongh <tongh@tonghdeMacBook-Pro.local>
2018-04-08 16:10:08 +0800

    edit

--- start.php
+++ start.php
@@ -9,44 +9,48 @@ include './lib/tree.php';
 
 class start{
     public $tree;
-    public $flag = 0;
+
+    public function __construct()
+    {
+        $tree = new TrieTree();
+        $this->tree = $tree;
+    }
 
     public function startServer(){
         $http = new swoole_http_server("127.0.0.1", 9501);
-        
-        $http->on('request', function ($request, $response) {
-            if ($this->flag == 0){
-                $tree = new TrieTree();
 
-                $myfile = fopen("./lib/words/words.txt", "r") or die("Unable to open file!");
-                while(!feof($myfile)) {
-                    $content = fgets($myfile);
-                    $tree->insert($content);
-                }
-                fclose($myfile);
+        $http->on('WorkerStart',[$this,'workStart']);
+        $http->on('request',[$this,'request']);
 
-                $this->tree = $tree;
-                $this->flag = 1;
-            }
-
-            $uri = $request->server['request_uri'];
-
-            if ($uri != '/favicon.ico'){
-                $word = $request->get['q'];
-//                $res = $this->tree->find($word);
-                $res = $this->tree->contain($word);
-                if ($res == false){
-                    $msg = json_encode(['code'=>10000,'filter'=>'','result'=>$word]);
-                }else{
-                    foreach ($res as $item){
-                        $word = str_replace($item,str_pad('', mb_strlen($item),'*') , $word);
-                    }
-                    $msg = json_encode(['code'=>10000,'filter'=>$res,'result'=>$word]);
-                }
-                $response->end($msg);
-            }
-        });
         $http->start();
+    }
+
+    public function workStart($server){
+        $myfile = fopen("./lib/words/words.txt", "r") or die("Unable to open file!");
+        while(!feof($myfile)) {
+            $content = trim(fgets($myfile));
+            $this->tree->insert($content);
+        }
+        fclose($myfile);
+    }
+
+    public function request($request, $response){
+        $uri = $request->server['request_uri'];
+
+        if ($uri != '/favicon.ico'){
+            $word = $request->get['q'];
+            $res = $this->tree->contain($word);
+            if ($res == false){
+                $msg = json_encode(['code'=>10000,'filter'=>'','result'=>$word]);
+            }else{
+                foreach ($res as $item){
+                    $word = str_replace($item,str_pad('', mb_strlen($item),'*') , $word);
+                }
+                $msg = json_encode(['code'=>10000,'filter'=>$res,'result'=>$word]);
+            }
+            $response->end($msg);
+        }
+
     }
 }
 
